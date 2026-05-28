3af9425503d2144052330aaf24e681ebf0ca3363
PRACHI GUPTA <prachisrgupta03@gmail.com>
2026-05-28 12:32:49 +0530

    Time: 3 ms (86.78%), Space: 52.6 MB (21.69%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,14 +1,14 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0 ;
+        int n = 0 ;
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
                 if(grid[i][j] == '0') continue ;
                  fn(grid, i, j) ;
-                 count++ ;
+                 n++ ;
             }
         }
-        return count ;
+        return n ;
     }
     public void fn(char[][] grid, int r, int c){
         if(r<0 || r>grid.length-1 || c<0 || c>grid[0].length-1 || grid[r][c] == '0') return ;
