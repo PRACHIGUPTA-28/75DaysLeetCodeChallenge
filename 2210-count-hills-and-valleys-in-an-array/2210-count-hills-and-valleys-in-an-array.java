5ccb9bc96f2997af8a87ee90f64d64bd9b17de06
PRACHI GUPTA <prachisrgupta03@gmail.com>
2026-08-14 12:49:50 +0530

    Time: 0 ms (100%), Space: 43.1 MB (87.34%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
@@ -1,27 +1,12 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int[] left = new int[nums.length];
-        left[0] = -1 ;
-        left[nums.length-1] = -1 ;
-        for(int i=1; i<nums.length; i++){
-            if(nums[i] > nums[i-1]) left[i] = 0 ;
-            else if(nums[i] < nums[i-1]) left[i] = 1 ;
-            else left[i] = left[i-1] ;
-        }
-        int[] right = new int[nums.length];
-        right[0] = -1 ;
-        right[nums.length-1] = -1 ;
-        for(int i=nums.length-2; i>=0; i--){
-            if(nums[i] > nums[i+1]) right[i] = 0 ;
-            else if(nums[i] < nums[i+1]) right[i] = 1 ;
-            else right[i] = right[i+1] ;
-        }
+        int prev = nums[0] ;
         int n = 0 ;
         for(int i=1; i<nums.length-1; i++){
-            if(left[i] == -1 || right[i] == -1) continue ;
-            if(left[i] == right[i]){
-                if(nums[i] != nums[i-1]) n++ ;
-            }
+            if(nums[i] == nums[i+1]) continue ;
+            if(nums[i] > prev && nums[i] > nums[i+1]) n++ ;
+            else if(nums[i] < prev && nums[i] < nums[i+1]) n++ ;
+            prev = nums[i] ;
         }
         return n ;
     }
